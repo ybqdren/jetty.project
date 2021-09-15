9ebb6b9f010f38d3a9bb220e5a092a70901f4b8c
Greg Wilkins <gregw@webtide.com>
2021-09-15 16:32:47 +1000

    simpler impl
    Signed-off-by: Greg Wilkins <gregw@webtide.com>

--- jetty-nested/src/main/java/org/eclipse/jetty/nested/NestedChannel.java
+++ jetty-nested/src/main/java/org/eclipse/jetty/nested/NestedChannel.java
@@ -31,24 +31,14 @@ import org.eclipse.jetty.util.IO;
 public class NestedChannel extends HttpChannel implements ReadListener
 {
     private static final int BUFFER_SIZE = 1024;
+    public static final HttpInput.EofContent EOF = new HttpInput.EofContent();
 
     private final HttpInput _httpInput;
     private final ServletInputStream _inputStream;
     private final Callback _asyncCompleteCallback;
     private final ByteBufferPool _bufferPool;
 
-    private ReadState _state = ReadState.AWAITING_DATA;
-    private boolean _inputEOF;
-    private Throwable _error;
-
-    private enum ReadState
-    {
-        AWAITING_DATA,
-        NEED_DATA,
-        DATA_AVAILABLE,
-        ERROR,
-        EOF
-    }
+    NestedContent _content;
 
     public NestedChannel(NestedConnector connector, HttpConfiguration configuration, NestedEndpoint endPoint, NestedTransport transport, Callback asyncCompleteCallback) throws IOException
     {
@@ -64,72 +54,38 @@ public class NestedChannel extends HttpChannel implements ReadListener
     @Override
     public boolean needContent()
     {
-        synchronized (this)
-        {
-            switch (_state)
-            {
-                case NEED_DATA:
-                    return false;
-
-                case AWAITING_DATA:
-                    _state = ReadState.NEED_DATA;
-                    return false;
-
-                case DATA_AVAILABLE:
-                case ERROR:
-                case EOF:
-                    return true;
-
-                default:
-                    throw new IllegalStateException(_state.name());
-            }
-        }
+        return _httpInput.isReady();
     }
 
     @Override
     public HttpInput.Content produceContent()
     {
-        boolean produce = false;
-        synchronized (this)
+        if (_inputStream.isFinished())
+            return EOF;
+
+        try
         {
-            switch (_state)
-            {
-                case DATA_AVAILABLE:
-
-                    if (_inputEOF)
-                    {
-                        _state = ReadState.EOF;
-                        return new HttpInput.EofContent();
-                    }
-
-                    if (_inputStream.isReady())
-                        produce = true;
-                    else
-                        _state = ReadState.AWAITING_DATA;
-                    break;
-
-                case EOF:
-                    return new HttpInput.EofContent();
-
-                case ERROR:
-                    return new HttpInput.ErrorContent(_error);
-            }
+            if (_content == null)
+                _content = new NestedContent(_bufferPool);
+            _content.readFrom(_inputStream);
+        }
+        catch (IOException e)
+        {
+            _content.failed(e);
+            _content = null;
+            fail(e);
+            return new HttpInput.ErrorContent(e);
         }
 
-        if (produce)
+        if (_content.hasContent())
         {
-            NestedContent content = new NestedContent(_bufferPool);
-            try
-            {
-                content.readFrom(_inputStream);
-            }
-            catch (IOException e)
-            {
-                fail(e);
-                return new HttpInput.ErrorContent(e);
-            }
+            HttpInput.Content content = _content;
+            _content = null;
             return content;
         }
+
+        if (_inputStream.isFinished())
+            return EOF;
 
         return null;
     }
@@ -137,41 +93,11 @@ public class NestedChannel extends HttpChannel implements ReadListener
     @Override
     public boolean failAllContent(Throwable failure)
     {
-        synchronized (this)
-        {
-            switch (_state)
-            {
-                case EOF:
-                    return true;
-
-                case ERROR:
-                    _error.addSuppressed(failure);
-                    return false;
-
-                default:
-                    _state = ReadState.ERROR;
-                    _error = failure;
-                    return false;
-            }
-        }
+        return _inputStream.isFinished();
     }
 
     private void fail(Throwable failure)
     {
-        synchronized (this)
-        {
-            switch (_state)
-            {
-                case ERROR:
-                    _error.addSuppressed(failure);
-                    break;
-
-                default:
-                    _state = ReadState.ERROR;
-                    _error = failure;
-                    break;
-            }
-        }
     }
 
     @Override
@@ -184,58 +110,15 @@ public class NestedChannel extends HttpChannel implements ReadListener
     @Override
     protected boolean eof()
     {
-        synchronized (this)
-        {
-            _inputEOF = true;
-            switch (_state)
-            {
-                case NEED_DATA:
-                case AWAITING_DATA:
-                    _state = ReadState.DATA_AVAILABLE;
-                    break;
-
-                default:
-                    break;
-            }
-        }
-
         return _httpInput.onContentProducible();
     }
 
     @Override
     public void onDataAvailable()
     {
-        boolean isContentProducible = false;
-        synchronized (this)
-        {
-            switch (_state)
-            {
-                case DATA_AVAILABLE:
-                    throw new IllegalStateException();
-
-                case AWAITING_DATA:
-                    if (_inputStream.isReady())
-                        _state = ReadState.DATA_AVAILABLE;
-                    break;
-
-                case NEED_DATA:
-                    isContentProducible = _inputStream.isReady();
-                    if (isContentProducible)
-                        _state = ReadState.DATA_AVAILABLE;
-                    break;
-
-                default:
-                    // Do nothing.
-                    break;
-            }
-        }
-
-        if (isContentProducible)
-        {
-            boolean handle = _httpInput.onContentProducible();
-            if (handle)
-                execute(this);
-        }
+        boolean handle = _httpInput.onContentProducible();
+        if (handle)
+            execute(this);
     }
 
     @Override
